178af3270435d1a8551de5f5014a1e263bf6511d
Mustajab Ali <mustajabali.321@gmail.com>
2023-10-10 22:42:43 +0500

    Updated for better performance

--- app.js
+++ app.js
@@ -1,49 +1,47 @@
-let start = document.getElementById("start");
-let reset = document.getElementById("reset");
+let hourInput = document.getElementById("hour");
+let minuteInput = document.getElementById("minute");
+let secondInput = document.getElementById("second");
+let startBtn = document.getElementById("startBtn");
+let resetBtn = document.getElementById("resetBtn");
+let timerInterval;
 
-let h = document.getElementById("hour");
-let m = document.getElementById("minute");
-let s = document.getElementById("second");
+startBtn.addEventListener("click", function () {
+    let hours = parseInt(hourInput.value) || 0;
+    let minutes = parseInt(minuteInput.value) || 0;
+    let seconds = parseInt(secondInput.value) || 0;
 
-let startTimer = null;
+    // Ensure hours, minutes, and seconds are not less than zero
+    hours = Math.max(0, hours);
+    minutes = Math.max(0, minutes);
+    seconds = Math.max(0, seconds);
 
-start.addEventListener('click',startFunction);
-reset.addEventListener('click',resetFunction);
-
-function timer(){
-    if (h.value == 0 && m.value ==0 && s.value==0){
-        h.value = 0;
-        m.value = 0;
-        s.value = 0;
+    let totalSeconds = hours * 3600 + minutes * 60 + seconds;
+    if (totalSeconds > 0 && !timerInterval) {
+        startTimer(totalSeconds);
     }
-    else if(s.value != 0){
-        s.value--;
-    }
-    else if(m.value != 0 && s.value == 0){
-        s.value = 59;
-        m.value--;
-    }
-    else if(h.value != 0 && m.value == 0){
-        m.value = 60;
-        h.value--;
-    }
-} 
+});
 
-function stopTimer(){
-    clearInterval(startTimer);
-}
+resetBtn.addEventListener("click", function () {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    hourInput.value = "";
+    minuteInput.value = "";
+    secondInput.value = "";
+});
 
-function startFunction(){
-    startInterval();
-}
-
-function startInterval(){
-    startTimer = setInterval(function(){timer()},1000);
-}
-
-function resetFunction(){
-    h.value = 0;
-    m.value = 0;
-    s.value = 0;
-    stopTimer(); 
+function startTimer(totalSeconds) {
+    timerInterval = setInterval(function () {
+        if (totalSeconds <= 0) {
+            clearInterval(timerInterval);
+            alert("Time's up!");
+        } else {
+            totalSeconds--;
+            let hours = Math.floor(totalSeconds / 3600);
+            let minutes = Math.floor((totalSeconds % 3600) / 60);
+            let seconds = totalSeconds % 60;
+            hourInput.value = hours < 10 ? "0" + hours : hours;
+            minuteInput.value = minutes < 10 ? "0" + minutes : minutes;
+            secondInput.value = seconds < 10 ? "0" + seconds : seconds;
+        }
+    }, 1000);
 }
